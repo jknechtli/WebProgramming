58a03c51ff418d5195f588241ae3e1344f024005
Joel Knechtli <j.knechtli@valdperformance.com>
2019-08-07 12:44:10 +1000

    Added server to week5

--- week3/routes/accountroute.js
+++ week3/routes/accountroute.js
@@ -1,4 +1,9 @@
 module.exports = (app, path) => {
+  // app passes in the express object needed for the route.
+  // path passes in a path object needed to find the file. The
+  // path module is part of node and needs to be required in the
+  // server.js file 
+
   app.get('/account', (req, res) => {
     const filepath = path.resolve('/www/account.html');
     res.sendFile(filepath);
